6f78f8de6e17d5b3cb320c7e12bb054a0315cc46
mohamed amine bouchalkha <mohamed.bouchalkha@exova.com>
2018-05-31 18:07:05 -0400

    Init VuePress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  title: 'Netlify CMS + VuePress',
+  description: 'Netlify + VuePress',
+  themeConfig: {
+    docsDir: 'docs',
+    repo: 'mabouchalkha/VueJsPress',
+    sidebar: [
+      '/',
+      '/welcome',
+      '/test'
+    ],
+    nav: [
+      {
+        text: 'Admin',
+        link: '/admin/#/',
+      }
+    ]
+  }
+}
